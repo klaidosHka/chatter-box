ae808a7effff2b2a95fa4299c290722ee446a415
klaidosHka <klaidukast17@gmail.com>
2023-03-26 22:59:57 +0300

    MAIN-PAGE Added default profile circle avatar; Updated main Index page to take user list.

--- ChatterBox.Application/Areas/Main/Pages/Index.cshtml.cs
+++ ChatterBox.Application/Areas/Main/Pages/Index.cshtml.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ChatterBox.Application.Areas.Main.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
 
+        public IndexModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IEnumerable<UserViewModel> GetUsers()
+        {
+            return _userManager.Users
+                .Select(u => new UserViewModel
+                {
+                    Username = u.UserName.Split('@', StringSplitOptions.RemoveEmptyEntries)[0],
+                    Status = "Offline"
+                })
+                .ToList();
+        }
+
+        public async Task<IdentityUser> GetCurrentUserAsync()
+        {
+            return await _userManager.GetUserAsync(HttpContext.User);
+        }
+    }
+
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+
+        public string Status { get; set; }
     }
 }
