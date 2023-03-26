6967d4dc666b26d44fd41d41ebb41a3594564855
tomakas54 <112956264+tomakas54@users.noreply.github.com>
2023-03-26 13:03:42 +0300

    Implemented, main chat structure, user menu
    Implemented, main chat structure with messages, user menu with names and profile pictures, profile search bar

---
+++ ChatterBox.Application/Pages/Main.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatterBox.Application.Pages
+{
+    public class MainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
