441560c61677f6e48c52da9e4cd8b47bd4eeafa7
tomakas54 <112956264+tomakas54@users.noreply.github.com>
2023-05-11 14:12:59 +0300

    Dropdown menu
    Made a Dropdown menu, whenever you hover on user's profile in the top right corner

---
+++ ChatterBox.Application/wwwroot/js/dropDownMenu.js
@@ -0,0 +1,11 @@
+﻿    $(document).ready(function() {
+        $('.dropdown-toggle').click(function (e) {
+            e.stopPropagation();
+            $('.dropdown-menu').hide();
+            $(this).siblings('.dropdown-menu').toggle();
+        });
+
+    $(document).click(function() {
+        $('.dropdown-menu').hide();
+        });
+    });
